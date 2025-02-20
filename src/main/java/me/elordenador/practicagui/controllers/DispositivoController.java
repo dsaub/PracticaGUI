75758dc33effe9bb397696cca0485f6410bdaa76
Daniel <danilacasito8@gmail.com>
2025-02-20 12:16:21 +0100

    Create the controller and the UI

---
+++ src/main/java/me/elordenador/practicagui/controllers/DispositivoController.java
@@ -0,0 +1,29 @@
+package me.elordenador.practicagui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DispositivoController implements Initializable {
+    @FXML
+    private void addDevice(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void deleteDevice(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void editDevice(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
