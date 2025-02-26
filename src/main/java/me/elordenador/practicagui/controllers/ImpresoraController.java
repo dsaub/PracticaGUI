0341fde64805c420c91c09c08dbd241c52a8add9
Daniel <danilacasito8@gmail.com>
2025-02-26 13:44:44 +0100

    Add "Impresora" FXML and Basic Controller

---
+++ src/main/java/me/elordenador/practicagui/controllers/ImpresoraController.java
@@ -0,0 +1,17 @@
+package me.elordenador.practicagui.controllers;
+
+import javafx.event.ActionEvent;
+
+public class ImpresoraController {
+    public void search(ActionEvent actionEvent) {
+    }
+
+    public void addDevice(ActionEvent actionEvent) {
+    }
+
+    public void editDevice(ActionEvent actionEvent) {
+    }
+
+    public void deleteDevice(ActionEvent actionEvent) {
+    }
+}
